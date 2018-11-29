5283497255e4c43233ab16c4958df1b76fad1b48
ShashankBodkhe <shashank_bansal09@yahoo.co.in>
2018-11-29 14:49:29 +0530

    orignal1

--- index.js
+++ index.js
@@ -13,20 +13,6 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-// restService.post("/echo", function(req, res) {
-//   var speech =
-//     req.body.result &&
-//     req.body.result.parameters &&
-//     req.body.result.parameters.echoText
-//       ? req.body.result.parameters.echoText
-//       : "Seems like some problem. Speak again.";
-//   return res.json({
-//     speech: speech,
-//     displayText: speech,
-//     source: "webhook-echo-sample"
-//   });
-// });
-
 restService.post("/echo", function(req, res) {
   var speech =
     req.body.result &&
@@ -35,11 +21,13 @@ restService.post("/echo", function(req, res) {
       ? req.body.result.parameters.echoText
       : "Seems like some problem. Speak again.";
   return res.json({
-   fulfillmentText : speech,
-    
+    speech: speech,
+    displayText: speech,
     source: "webhook-echo-sample"
   });
 });
+
+
 
 restService.post("/audio", function(req, res) {
   var speech = "";
