aaa06db3d3a43118c077eed673645f545c88493a
ShashankBodkhe <shashank_bansal09@yahoo.co.in>
2018-11-29 15:39:56 +0530

    change 3

--- index.js
+++ index.js
@@ -20,11 +20,21 @@ restService.post("/echo", function(req, res) {
     req.body.result.parameters.echoText
       ? req.body.result.parameters.echoText
       : "Seems like some problem. Speak again.";
-  return res.json({
+  return res.json(
+  {
     fulfillmentText: speech,
-   
-    source: "webhook-echo-sample"
-  });
+    fulfillmentMessages:
+     [
+    {
+      text: [
+        speech
+      ],
+    }
+     ],
+    source: "example.com"
+  }
+  
+  );
 });
 
 
